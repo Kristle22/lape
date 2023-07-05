68c4c80d8364fb5d3ed1eeb6ce79cf94d2166c3b
Kristina <crislayn@yahoo.com>
2023-07-05 13:46:16 +0300

    remembering useState&lists

--- reax/src/App.jsx
+++ reax/src/App.jsx
@@ -1,16 +1,59 @@
+import { useState } from 'react';
 import './App.css';
-import Hello from './Components/009/Hello';
-import Kurmis from './Components/009/Kurmis';
+import './Functions/randColor';
+import randColor from './Functions/randColor';
+
 
 function App() {
+
+  const [spalva, setSpalva] = useState('skyblue');
+  const [skaicius, setSkaicius] = useState(1);
+
+  const [kv, setKv] = useState([]);
+
+  const add = () => setKv(k => [...k, randColor()]);
+  const remove = () => setKv(k => k.slice(1));
+
+  const cats = ['Pilkis', 'Rainis', 'Murkis'];
+
+  const miracle = (a = 'be') => {
+    console.log('Miracle of miracles! ' + a);
+    return anotherMiracle;
+  }
+
+  const anotherMiracle = () => {
+    console.log('Another Miracle of other miracles!');
+    // const newColor = spalva === 'skyblue' ? 'crimson' : 'skyblue';
+    setSpalva(oldColor => oldColor === 'skyblue' ? 'crimson' : 'skyblue');
+    console.log(spalva);
+  }
+
+  const counting = () => {
+    setSkaicius(c => c + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Hello name="Kristina"></Hello>
-        <Kurmis spalva="crimson" dydis={20} skaicius={4} />
-        <Hello name="Kurmi" />
-        <Kurmis spalva="skyblue" dydis={10} skaicius={4} />
-        <Hello name="Kurmi" />
+        <div className="kvc">
+          {
+            kv.map((k, i) => <div style={{ background: k }} className="kv" key={i}>{i}</div>)
+          }
+        </div>
+        <div className="kvc">
+          <button type="button" onClick={add}>ADD kv</button>
+          <button type="button" onClick={remove}>REM kv</button>
+        </div>
+        <div style={{ marginTop: '20px' }}>
+          {
+            cats.map((c, i) => <div key={i}>{c}</div>)
+          }
+        </div>
+        <h1>{skaicius}</h1>
+        <button type="button" onClick={counting}>Let's count!</button>
+        <h1 style={{ color: spalva }}>State</h1>
+        <button type="button" onClick={() => miracle('Abra-Cadabra')}>Click me WITH!</button>
+        <button type="button" onClick={anotherMiracle}>Click me W/O!</button>
       </header>
     </div>
   );
--- reax/src/index.js
+++ reax/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import App009 from './App009';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
+  // <App009 />
   <App />
   // </React.StrictMode>
 );
