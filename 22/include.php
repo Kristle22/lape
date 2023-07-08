0a17e04b111e07b5ee4794021a38cc3773ab88ad
Kristina <crislayn@yahoo.com>
2023-07-08 22:51:28 +0300

    require & returned files

--- 22/include.php
+++ 22/include.php
@@ -6,6 +6,11 @@ echo KYE;
 echo '<br>Here some files are included:<br>';
 echo __DIR__.'./kitas/vienas.php';
 
-include __DIR__.'./kitas/vienas.php';
-// include __DIR__.'./index.php';
-include __DIR__.'./trecias/du.php';
+require __DIR__.'./kitas/vienas.php';
+require __DIR__.'./kitas/vienas.php';
+// include_once __DIR__.'./index.php';
+require __DIR__.'./trecias/du.php';
+
+$data = require __DIR__.'/data.php';
+
+print_r($data);
