48df159478e2eb1444905b5140ec2329ceb2cbaa
Kristina <crislayn@yahoo.com>
2023-09-23 21:05:07 +0300

    data rendering in React

--- reax2/src/App.jsx
+++ reax2/src/App.jsx
@@ -2,13 +2,25 @@ import './App.css';
 import Create from './Components/Create';
 import List from './Components/List';
 import './bootstrap.css';
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 function App() {
+
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost/lape/reax2server/animals').then(res => {
+      console.log(res);
+      setAnimals(res.data);
+    });
+  }, []);
+
   return (
     <div className="container">
       <div className="row">
         <Create />
-        <List />
+        <List animals={animals} />
       </div>
     </div>
   );
--- reax2/src/Components/List.jsx
+++ reax2/src/Components/List.jsx
@@ -1,12 +1,17 @@
-function List() {
+function List({ animals }) {
   return (
     <div className="col-7">
       <div className="card mt-4">
         <div className="card-header">
-          <h2>List</h2>
+          <h2>List of animals</h2>
         </div>
         <div className="card-body">
-
+          {
+            animals.map((c, i) => <div key={i}> <b style={{
+              textTransform: 'capitalize'
+            }}
+            >{c.animal}</b>: {c.weight} kg</div>)
+          }
         </div>
       </div>
     </div>
