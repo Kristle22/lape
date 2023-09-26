c9088810e038e5ad26a4d276866db43075c927d5
Kristina <crislayn@yahoo.com>
2023-09-26 13:51:29 +0300

    modal centered

--- reax2/src/Components/Edit.jsx
+++ reax2/src/Components/Edit.jsx
@@ -2,7 +2,7 @@ function Edit() {
 
   return (
     <div class="modal">
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title">Modal title</h5>
